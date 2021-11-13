2204d193aec1e54be6a5353f6bf512a55fcf9a7e
Ilya Nikitin <f1uctus@mail.ru>
2021-11-14 02:45:57 +0300

    Update `hibernate-types` to 2.14.0

--- bloom-core/build.gradle.kts
+++ bloom-core/build.gradle.kts
@@ -4,7 +4,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("com.fasterxml.jackson.core:jackson-databind")
-    implementation("com.vladmihalcea:hibernate-types-55:2.13.0")
+    implementation("com.vladmihalcea:hibernate-types-55:2.14.0")
 
     runtimeOnly("com.h2database:h2")
     runtimeOnly("io.r2dbc:r2dbc-h2")
