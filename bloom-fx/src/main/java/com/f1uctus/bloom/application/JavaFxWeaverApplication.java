bd69d92e5549745ab8732edd69d8dd2ec5c593ad
Ilya Nikitin <ilya.i.nikitin@mail.ru>
2021-12-05 19:05:10 +0300

    Add Pattern <-> String adapters for FXForm2

--- bloom-fx/src/main/java/com/f1uctus/bloom/application/JavaFxWeaverApplication.java
+++ bloom-fx/src/main/java/com/f1uctus/bloom/application/JavaFxWeaverApplication.java
@@ -1,8 +1,14 @@
 package com.f1uctus.bloom.application;
 
+import com.dooapp.fxform.adapter.*;
+import com.dooapp.fxform.handler.WrappedTypeHandler;
+import com.dooapp.fxform.view.factory.DefaultFactoryProvider;
+import com.dooapp.fxform.view.factory.impl.TextFieldFactory;
 import com.f1uctus.bloom.core.persistence.models.User;
 import com.f1uctus.bloom.core.persistence.repositories.UserRepository;
 import javafx.application.Application;
+import javafx.beans.property.StringProperty;
+import javafx.beans.property.adapter.JavaBeanObjectProperty;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.availability.ApplicationAvailabilityAutoConfiguration;
@@ -12,10 +18,12 @@ import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 @SpringBootApplication(scanBasePackages = "com.f1uctus.bloom", exclude = {
     TaskSchedulingAutoConfiguration.class,
@@ -58,5 +66,31 @@ public class JavaFxWeaverApplication {
                 )
             ));
         };
+    }
+
+    @Bean
+    CommandLineRunner fxFormInitializer(List<Converter<?, ?>> converters) {
+        return args -> {
+            DefaultAdapterProvider.addGlobalAdapter(
+                new TypeAdapterMatcher(JavaBeanObjectProperty.class, StringProperty.class),
+                new PatternToStringAdapter()
+            );
+            DefaultFactoryProvider.addGlobalFactory(
+                new WrappedTypeHandler(Pattern.class),
+                new TextFieldFactory()
+            );
+        };
+    }
+
+    static class PatternToStringAdapter implements Adapter<Pattern, String> {
+        @Override
+        public String adaptTo(Pattern from) {
+            return from.toString();
+        }
+
+        @Override
+        public Pattern adaptFrom(String to) {
+            return Pattern.compile(to);
+        }
     }
 }
