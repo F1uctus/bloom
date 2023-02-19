29913c5c467edbdc0d588f278738a48471a76f75
Ilya Nikitin <ilya.i.nikitin@proton.me>
2023-02-19 19:41:50 +0300

    Simplify SpeechPluginImpl

--- bloom-plugins/speech/src/main/java/com/f1uctus/bloom/plugins/speech/SpeechPlugin.java
+++ bloom-plugins/speech/src/main/java/com/f1uctus/bloom/plugins/speech/SpeechPlugin.java
@@ -10,6 +10,7 @@ import com.f1uctus.bloom.plugins.speech.events.SpeechEventActivationPattern;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
@@ -18,10 +19,11 @@ import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import lombok.SneakyThrows;
+import org.jetbrains.annotations.NotNull;
 import org.pf4j.*;
 import reactor.adapter.JdkFlowAdapter;
 
-import java.util.*;
+import java.util.Properties;
 import java.util.concurrent.Flow;
 
 public class SpeechPlugin extends Plugin {
@@ -47,47 +49,58 @@ public class SpeechPlugin extends Plugin {
     @Extension
     public static class SpeechPluginImpl implements EventPlugin<SpeechEvent>, AuthPlugin {
         private PluginHost host;
-        private static final Map<Label, Disposable> subscriptions = new HashMap<>();
+        private Disposable subscription;
 
-        @Override public Node buildAuthorizationView() {
-            var button = new ToggleButton("Speech");
+        public SpeechPluginImpl(PluginHost host) {
+            this.host = host;
+        }
+
+        @Override
+        public Node buildAuthorizationView() {
+            var toggle = new ToggleButton("Speech");
             var label = new Label();
             label.setFont(new Font(14));
             label.setText("Произнесите ключевую фразу");
             label.managedProperty().bind(label.visibleProperty());
-            label.visibleProperty().bind(button.selectedProperty());
-            label.visibleProperty().addListener((o, s, u) -> {
-                if (u) {
-                    subscriptions.put(
-                        label,
-                        Reactives.toObservable(events())
-                            .subscribeOn(Schedulers.computation())
-                            .filter(e -> !e.getText().equals(label.getText()))
-                            .subscribe(e -> Platform.runLater(() -> {
-                                label.setText(e.getText());
-                                var holder = host.getIdentityRepository()
-                                    .findByIdentityHash(label.getText());
-                                if (holder.isPresent()) {
-                                    label.setStyle("-fx-text-fill: green");
-                                    button.setSelected(false);
-                                    host.setIdentityHolder(holder.get());
-                                } else {
-                                    label.setStyle("-fx-text-fill: red");
-                                }
-                            }))
-                    );
-                } else if (subscriptions.containsKey(label)) {
-                    subscriptions.remove(label).dispose();
-                }
-            });
-            var box = new HBox(button, label);
+            label.visibleProperty().bind(toggle.selectedProperty());
+            label.visibleProperty().addListener(buildUIVisibleListener(toggle, label));
+            var box = new HBox(toggle, label);
             box.setAlignment(Pos.CENTER);
             box.setSpacing(10);
             box.setPadding(new Insets(5));
             return box;
         }
 
-        @Override public Flow.Publisher<SpeechEvent> events() {
+        @NotNull
+        private ChangeListener<Boolean> buildUIVisibleListener(
+            ToggleButton authBySpeechToggle,
+            Label recognizedSpeechLabel
+        ) {
+            return (observable, wasVisible, nowVisible) -> {
+                if (subscription != null) {
+                    subscription.dispose();
+                    subscription = null;
+                }
+                if (!nowVisible)
+                    return;
+                subscription = Reactives.toObservable(events())
+                    .subscribeOn(Schedulers.computation())
+                    .filter(ev -> !ev.getText().equals(recognizedSpeechLabel.getText()))
+                    .subscribe(ev -> Platform.runLater(() -> {
+                        recognizedSpeechLabel.setText(ev.getText());
+                        host.getIdentityRepository()
+                            .findByIdentityHash(recognizedSpeechLabel.getText())
+                            .ifPresentOrElse(holder -> {
+                                recognizedSpeechLabel.setStyle("-fx-text-fill: green");
+                                authBySpeechToggle.setSelected(false);
+                                host.setIdentityHolder(holder);
+                            }, () -> recognizedSpeechLabel.setStyle("-fx-text-fill: red"));
+                    }));
+            };
+        }
+
+        @Override
+        public Flow.Publisher<SpeechEvent> events() {
             return JdkFlowAdapter.publisherToFlowPublisher(
                 recognizer.startListening().filter(e -> !e.getText().isBlank())
             );
